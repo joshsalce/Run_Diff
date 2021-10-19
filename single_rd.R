7bfd3916b1ec9a9a78462bafd6b716197e2cef83
JoshSalce <65614069+joshsalce@users.noreply.github.com>
2021-10-18 20:23:39 -0700

    Update single_rd.R

--- single_rd.R
+++ single_rd.R
@@ -7,14 +7,13 @@ cont_RD = function(team, year) {
   d4t4 <- getRetrosheet("game", year)
   subject <- subset(d4t4, d4t4$HmTm == team | d4t4$VisTm == team)
   v = c()
-  initial_rd = 0
+  run_diff = 0
   
   for (i in 1:nrow(subject)) {
-    run_diff <- initial_rd + ifelse(subject$HmTm[i] == team, 
+    run_diff <- run_diff + ifelse(subject$HmTm[i] == team, 
                                     subject$HmRuns[i] - subject$VisRuns[i], 
                                     subject$VisRuns[i] - subject$HmRuns[i]) 
     v = c(v, run_diff)
-    initial_rd <- run_diff
   }
   print(v)
   
