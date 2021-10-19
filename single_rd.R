43d304acc545c4e8e3b705d3a5d4a36608914177
JoshSalce <65614069+joshsalce@users.noreply.github.com>
2021-10-18 20:14:43 -0700

    Update single_rd.R

--- single_rd.R
+++ single_rd.R
@@ -6,17 +6,18 @@ library(tidyverse)
 cont_RD = function(team, year) {
   d4t4 <- getRetrosheet("game", year)
   subject <- subset(d4t4, d4t4$HmTm == team | d4t4$VisTm == team)
-  
   v = c()
   
   for (i in 1:nrow(subject)) {
-    run_diff <- initial_rd + ifelse(subject$HmTm[i] == team, subject$HmRuns[i] - subject$VisRuns[i], subject$VisRuns[i] - subject$HmRuns[i]) 
+    run_diff <- initial_rd + ifelse(subject$HmTm[i] == team, 
+                                    subject$HmRuns[i] - subject$VisRuns[i], 
+                                    subject$VisRuns[i] - subject$HmRuns[i]) 
     v = c(v, run_diff)
     initial_rd <- run_diff
   }
   print(v)
   
-  games = c(1:length(v)) #if the amount of games played were greater than or less than 162
+  games = c(1:length(v)) #for the occasional Game 163
   ggplot(subject, aes(games, v)) +
     ylim (-300, 300) +
     xlab("Games") +
@@ -25,5 +26,4 @@ cont_RD = function(team, year) {
 }
 
 cont_RD("LAN", 2019)
-
-
+#FIN
